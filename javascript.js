f395bf2e8f46bdee2cdc9153a681699551444c01
celestine wainaina <wangaricelestine036@gmail.com>
2026-03-31 12:22:49 +0300

    implement nested arrow function for login tracker and remodify the whole code

--- javascript.js
+++ javascript.js
@@ -1,24 +1,23 @@
+function createLoginTracker() { 
 let attempt = 0;
 let isLocked = false;
 
-function login(password) {
-    if (isLocked) {
-        console.log("The account is locked. You cannot log in");
-        return; 
-    }
-
-    const correctPassword = "password123"; 
-
-    if (password === correctPassword) {
-        console.log("Login successful!");
-        attempt = 0;
-    } else {
-        attempt++;
-        console.log("Incorrect password. Attempt number: " + attempt);
-        
-        if (attempt >= 3) {
-            isLocked = true;
-            console.log("The account is now LOCKED after 3 failed attempts.");
-        }
-    }
+return(password) => {
+if (isLocked) {
+return "The account is locked .";
 }
+
+if(password === "userInfo.password") {
+attempt = 0;
+return "Login successful.";
+} else {
+attempt++;
+if (attempt >= 3) {
+isLocked = true;
+return "The account is now LOCKED after 3 failed attempts.";
+}
+return "Incorrect password. Attempt " + attempt + " of 3.";
+}
+};
+}
+
