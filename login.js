c25fbdd8dc02d5459f81187355521f8ba5e9982b
celestine wainaina <wangaricelestine036@gmail.com>
2026-04-02 16:48:56 +0300

    Fixed login tracker logic

--- login.js
+++ login.js
@@ -1,19 +1,19 @@
-function createLoginTracker(username){
+function createLoginTracker(celestine) {
     let attempts = 0;
     return () => {
         attempts++;
 
         if (attempts <= 3){
-            console.log('${username} attempt ${attempts}: Try again ');
+            console.log(`${celestine} attempt ${attempts}: Try again `);
         } else {
-            console.log('${username} account locked ');  
+            console.log(`${celestine} account locked `);  
         }
 
     };
 }
 
-const user= createLoginTracker('john_doe');
-user(); // john_doe attempt 1: Try again
-user(); // john_doe attempt 2: Try again
-user(); // john_doe attempt 3: Try again
-user(); // john_doe account locked
+const user= createLoginTracker('celestine');
+user(); // celestine attempt 1: Try again
+user(); // celestine attempt 2: Try again
+user(); // celestine  attempt 3: Try again
+user(); // celestine account locked
